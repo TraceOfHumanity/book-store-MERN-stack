7d79db0d819a2b72803fc5bbb7c9d521eb2ed347
Alexander_Ovchar <azalexxx02@gmail.com>
2024-07-19 16:29:05 +0300

    feat: Add MongoDB connection and update server startup
    This commit adds the necessary code to establish a connection to MongoDB using Mongoose. It also updates the server startup code to only start listening after a successful connection to the database is established.

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {PORT, mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -8,6 +9,12 @@ app.get("/", (req, res) => {
   return res.status(234).send("welcome to the homepage");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
