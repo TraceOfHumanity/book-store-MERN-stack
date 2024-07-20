6fdeb840803421cd4d79ca576a29dcac1a5390da
Alexander_Ovchar <azalexxx02@gmail.com>
2024-07-20 17:55:15 +0300

    update cors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,6 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
 app.use(cors({
   origin: "http://localhost:3000",
   methods: ["GET", "POST", "PUT", "DELETE"],
